7881d5be6b8638f790af2b846bbf6913be1a1a13
Stephen <Stephen@stephens-macbook-pro.local>
2018-11-21 21:39:55 -0500

    Initial commit

---
+++ LeaderBoardInterface.py
@@ -0,0 +1,55 @@
+from appJar import gui
+app = gui("Dragonball Fighterz Leaderd Board Creator")
+
+app.addLabelEntry("Left Side Text")
+app.addLabelEntry("Right Side Text")
+app.getEntry("Left Side Text")
+app.getEntry("Right Side Text")
+    
+
+#Choose number of players that will be present on leaderboard 
+app.addOptionBox("Number of Players", ["4", "5", "6", "7", "8", "9", "10"])
+for x in range(1, 5):
+    app.addLabelEntry("Player %d" % x)
+for y in range(1, (4*3)+1):
+    app.addLabelOptionBox("Character %d" % y, ["Android 16", "Anroid 17", "Android 18", 
+    "Android 21", "Bardock", "Beerus", "Broly", "Cell", "Cooler", "Frieza", "Ginyu", 
+    "Gohan (Adult)", "Gohan (Teen)", "Goku (Base)", "Goku (SSJ)", "Goku (SSB)", "Goku Black",
+    "Gotenks", "Hit", "Krillin", "Nappa", "Piccolo", "Tien", "Trunks", "Vegeta (Base)",
+    "Vegeta (SSJ)", "Vegeta (SSB)", "Vegito", "Yamcha", "Zamasu" ])    
+
+lastCount = 4  
+def addPlayerEntries():
+    global lastCount
+    y = int(app.getOptionBox("Number of Players")) 
+    if y == lastCount:
+        pass       
+    elif lastCount != y:
+        for x in range(1, y):
+            app.removeEntry("Player %d" % x)
+        for x in range(1, (lastCount*3)+1):
+            app.removeOptionBox("Character %d" % x)
+        for x in range(1, y+1):
+            app.addLabelEntry("Player %d" % x)
+            app.addCharacterChoices
+        for x in range(1,(y * 3)+1):
+            app.addLabelOptionBox("Character %d" % x, ["Android 16", "Anroid 17", "Android 18", 
+        "Android 21", "Bardock", "Beerus", "Broly", "Cell", "Cooler", "Frieza", "Ginyu", 
+        "Gohan (Adult)", "Gohan (Teen)", "Goku (Base)", "Goku (SSJ)", "Goku (SSB)", "Goku Black",
+        "Gotenks", "Hit", "Krillin", "Nappa", "Piccolo", "Tien", "Trunks", "Vegeta (Base)",
+        "Vegeta (SSJ)", "Vegeta (SSB)", "Vegito", "Yamcha", "Zamasu" ])
+    lastCount = y
+    print("%d" % lastCount)
+
+
+app.setOptionBoxChangeFunction("Number of Players", addPlayerEntries)
+#app.addButton("Create Image", createImage)
+#def getOptionBoxes(self)
+        #for x in app.getOptionBox("Number of Players") *3):
+            #app.getOptionBox("Character %d" x)
+                
+app.go()
+
+
+    
+    
